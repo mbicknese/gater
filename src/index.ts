f356fb7ab81421c9d8f810585f8edc6c03637395
Maarten Bicknese <maarten.bicknese@gmail.com>
2023-10-11 21:00:17 +0200

    feat!: add type safety to check
    BREAKING CHANGE: this feature swaps the schema and value argument

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,108 @@
-export function isValid(
-  value: Record<
-    string,
-    string | boolean | number | Array<unknown> | Record<string, unknown>
-  >,
-  schema: Record<string, "string" | "number" | "boolean" | "array" | "object">,
+type PossibleTypes = {
+  string: string;
+  number: number;
+  boolean: boolean;
+  array: Array<string | number | boolean>;
+  object: Record<string, string | number | boolean>;
+};
+
+type SchemaType<TKey extends string, TValue extends keyof PossibleTypes> = { [Property in TKey]: TValue };
+type ValueType<T extends SchemaType<string, keyof PossibleTypes>> = {
+  [Property in keyof T]: PossibleTypes[T[Property]];
+};
+
+/**
+ * Internal function to check key value against type
+ * @param value
+ * @param schema
+ */
+function _check(
+  value: Record<string, string | boolean | number | Array<unknown> | Record<string, unknown>>,
+  schema: Record<string, keyof PossibleTypes>,
 ): boolean {
+  // Return early when there's a length mismatch
+  // Note that length might be the same when there's both a surplus and missing field in the value
+  if (Object.keys(schema).length !== Object.keys(value).length) {
+    return false;
+  }
+
   for (const key in value) {
-    if (!(key in schema)) {
-      return false; // Excess keys not allowed
+    // Excess keys and empty values are not allowed
+    if (!(key in schema) || value[key] == null) {
+      return false;
     }
-    if (Array.isArray(value[key]) && schema[key] === "array") {
-      continue; // The odd one as Arrays are "objects"
+    // The odd one as Arrays are "objects"
+    if (Array.isArray(value[key])) {
+      if (schema[key] === "array") {
+        continue;
+      }
+      return false;
     }
+    // Not the right type
     if (typeof value[key] !== schema[key]) {
-      return false; // Not the right type
+      return false;
     }
+    // Whups, almost let that slide (NaN is a number, welcome to JavaScript)
     if (typeof value[key] === "number" && Number.isNaN(value[key])) {
-      return false; // Whups, almost let that slide
+      return false;
     }
   }
   return true;
+}
+
+/**
+ * Validates the value against given schema
+ *
+ * Schemas allow any string key, with one of the following types:
+ *  - string
+ *  - number
+ *  - boolean
+ *  - array
+ *  - object
+ *
+ * Values cannot be omitted or empty, nor can surplus values be supplied. Complex values do not allow for nesting.
+ *
+ * Example schema
+ * ```
+ *     {
+ *         brand: 'string',
+ *         model: 'string',
+ *         year:  'number'
+ *     }
+ * ```
+ * @param schema - Dictionary with all allowed keys and their prospective data type
+ * @param value - Dictionary with scalar data, flat arrays, or flat objects as values
+ */
+export function isValid<T extends SchemaType<string, keyof PossibleTypes>>(schema: T, value: ValueType<T>): boolean;
+/**
+ * Creates a validator that will validate given value against the schema
+ *
+ * Schemas allow any string key, with one of the following types:
+ *  - string
+ *  - number
+ *  - boolean
+ *  - array
+ *  - object
+ *
+ * Values cannot be omitted or empty, nor can surplus values be supplied. Complex values do not allow for nesting.
+ *
+ * Example schema
+ * ```
+ *     {
+ *         brand: 'string',
+ *         model: 'string',
+ *         year:  'number'
+ *     }
+ *```
+ * @param schema - Dictionary with all allowed keys and their prospective data type
+ * @return A validator function which takes a value to validate against the schema
+ */
+export function isValid<T extends SchemaType<string, keyof PossibleTypes>>(schema: T): (value: ValueType<T>) => boolean;
+export function isValid<T extends SchemaType<string, keyof PossibleTypes>>(schema: T, value?: ValueType<T>) {
+  function validate(value: ValueType<T>): boolean {
+    return _check(value, schema);
+  }
+
+  if (value) return validate(value);
+  return validate;
 }
--- tests/isValid.spec.ts
+++ tests/isValid.spec.ts
@@ -1,10 +1,15 @@
 import { isValid } from "../src/index.ts";
 
+// Need to have a reference for checking equality
+const emptyArray = [] as const;
+const emptyObject = {} as const;
+
+// A list of values that could cause havoc
 const wrongValues: Array<any> = [
   true,
   false,
-  [],
-  {},
+  emptyArray,
+  emptyObject,
   "",
   "Dell",
   0,
@@ -18,43 +23,123 @@ const wrongValues: Array<any> = [
 ];
 
 describe("isValid", () => {
-  describe("string fields", () => {
-    it("accepts empty string", () => {
-      expect(isValid({ brand: "" }, { brand: "string" })).toBe(true);
+  describe("logic", () => {
+    let untypedComputer: any;
+    it("returns a validator function on a single argument", () => {
+      expect(typeof isValid({ brand: "string" }) === "function").toBe(true);
     });
-    it("accepts a string with content", () => {
-      expect(isValid({ brand: "Apple" }, { brand: "string" })).toBe(true);
+    it("returns a boolean when called with two arguments", () => {
+      untypedComputer = { brand: 1 };
+      expect(isValid({ brand: "string" }, { brand: "Apple" })).toBe(true);
+      expect(isValid({ brand: "string" }, untypedComputer)).toBe(false);
     });
-
-    it.each(wrongValues.filter((value) => !["Dell", ""].includes(value)))(
-      "rejects %s",
-      (wrongValue) => {
-        expect(isValid({ brand: wrongValue }, { brand: "string" })).toBe(false);
-      },
-    );
+    it("can be curried", () => {
+      // We're actually testing JavaScript here, but it's good to have the regression and example.
+      untypedComputer = { brand: 1 };
+      expect(isValid({ brand: "string" })({ brand: "Apple" })).toBe(true);
+      expect(isValid({ brand: "string" })(untypedComputer)).toBe(false);
+    });
+    it("rejects missing fields", () => {
+      untypedComputer = { brand: "HP" };
+      expect(isValid({ brand: "string", model: "string" }, untypedComputer)).toBe(false);
+    });
+    it("rejects surplus fields", () => {
+      untypedComputer = { brand: "Lenovo", model: "Legion" };
+      expect(isValid({ brand: "string" }, untypedComputer)).toBe(false);
+    });
+    it("validates composite schemas", () => {
+      expect(
+        isValid(
+          { brand: "string", year: "number", latest: "boolean", metadata: "object", tags: "array" },
+          { brand: "Apple", year: 2021, latest: false, metadata: { owner: "Maarten" }, tags: ["Silicon"] },
+        ),
+      ).toBe(true);
+    });
   });
 
-  describe("number fields", () => {
-    it("accepts zero", () => {
-      expect(isValid({ amount: 0 }, { amount: "number" })).toBe(true);
-    });
-    it("accepts positive integers", () => {
-      expect(isValid({ brand: 42 }, { brand: "number" })).toBe(true);
-    });
-    it("accepts negative integers", () => {
-      expect(isValid({ brand: -42 }, { brand: "number" })).toBe(true);
-    });
-    it("accepts floats", () => {
-      expect(isValid({ brand: Math.PI }, { brand: "number" })).toBe(true);
-    });
-    it("accepts irrational numbers", () => {
-      expect(isValid({ brand: Math.E }, { brand: "number" })).toBe(true);
+  describe("types", () => {
+    describe("string fields", () => {
+      const stringValidator = isValid({ brand: "string" });
+
+      it("accepts empty string", () => {
+        expect(stringValidator({ brand: "" })).toBe(true);
+      });
+      it("accepts a string with content", () => {
+        expect(stringValidator({ brand: "Apple" })).toBe(true);
+      });
+
+      it.each(wrongValues.filter((value) => !["Dell", ""].includes(value)))("rejects %s", (wrongValue) => {
+        expect(stringValidator({ brand: wrongValue })).toBe(false);
+      });
     });
 
-    it.each(
-      wrongValues.filter((value) => ![0, -42, Math.PI, Math.E].includes(value)),
-    )("rejects %s", (wrongValue) => {
-      expect(isValid({ brand: wrongValue }, { brand: "number" })).toBe(false);
+    describe("number fields", () => {
+      const numberValidator = isValid({ amount: "number" });
+
+      it("accepts zero", () => {
+        expect(numberValidator({ amount: 0 })).toBe(true);
+      });
+      it("accepts positive integers", () => {
+        expect(numberValidator({ amount: 42 })).toBe(true);
+      });
+      it("accepts negative integers", () => {
+        expect(numberValidator({ amount: -42 })).toBe(true);
+      });
+      it("accepts floats", () => {
+        expect(numberValidator({ amount: Math.PI })).toBe(true);
+      });
+      it("accepts irrational numbers", () => {
+        expect(numberValidator({ amount: Math.E })).toBe(true);
+      });
+
+      it.each(wrongValues.filter((value) => ![0, -42, Math.PI, Math.E].includes(value)))("rejects %s", (wrongValue) => {
+        expect(numberValidator({ amount: wrongValue })).toBe(false);
+      });
+    });
+
+    describe("boolean fields", () => {
+      const booleanValidator = isValid({ pending: "boolean" });
+
+      it("accepts true", () => {
+        expect(booleanValidator({ pending: true })).toBe(true);
+      });
+      it("accepts false", () => {
+        expect(booleanValidator({ pending: false })).toBe(true);
+      });
+
+      it.each(wrongValues.filter((value) => ![true, false].includes(value)))("rejects %s", (wrongValue) => {
+        expect(booleanValidator({ pending: wrongValue })).toBe(false);
+      });
+    });
+
+    describe("array fields", () => {
+      const arrayValidator = isValid({ tags: "array" });
+
+      it("accepts an empty array", () => {
+        expect(arrayValidator({ tags: [] })).toBe(true);
+      });
+      it("accepts an array with mixed values", () => {
+        expect(arrayValidator({ tags: [1, "hello", false] })).toBe(true);
+      });
+
+      it.each(wrongValues.filter((value) => ![emptyArray].includes(value)))("rejects %s", (wrongValue) => {
+        expect(arrayValidator({ tags: wrongValue })).toBe(false);
+      });
+    });
+
+    describe("Object fields", () => {
+      const objectValidator = isValid({ metadata: "object" });
+
+      it("accepts an empty object", () => {
+        expect(objectValidator({ metadata: {} })).toBe(true);
+      });
+      it("accepts an object with mixed values", () => {
+        expect(objectValidator({ metadata: { brand: "Apple", amount: 42, pending: false } })).toBe(true);
+      });
+
+      it.each(wrongValues.filter((value) => ![emptyObject].includes(value)))("rejects %s", (wrongValue) => {
+        expect(objectValidator({ metadata: wrongValue })).toBe(false);
+      });
     });
   });
 });
